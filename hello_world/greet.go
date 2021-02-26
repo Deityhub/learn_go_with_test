b50572095a29f1e8a951cee0e970597e953f6c14
Ojini Chizoba Jude <judoc96@gmail.com>
2021-02-26 16:51:15 +0100

    integrate language options into greeting

--- hello_world/greet.go
+++ hello_world/greet.go
@@ -2,16 +2,28 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const (
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+	spanish            = "Spanish"
+	french             = "French"
+)
 
 // Greet the user
-func Greet(name string) string {
+func Greet(name string, lang string) string {
 	if name == "" {
 		name = "World"
+	}
+	if lang == spanish {
+		return spanishHelloPrefix + name
+	}
+	if lang == french {
+		return frenchHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Greet("Michael"))
+	fmt.Println(Greet("Michael", "English"))
 }
--- hello_world/greet_test.go
+++ hello_world/greet_test.go
@@ -11,15 +11,27 @@ func TestGreeting(t *testing.T) {
 		}
 	}
 
-	t.Run("Greet someone (Jude)", func(t *testing.T) {
-		got := Greet("Jude")
+	t.Run("should greet someone (Jude)", func(t *testing.T) {
+		got := Greet("Jude", "")
 		want := "Hello, Jude"
 		assertion(t, got, want)
 	})
 
-	t.Run("say 'Hello, World' when empty string is supplied", func(t *testing.T) {
-		got := Greet("")
+	t.Run("should say 'Hello, World' when empty string is supplied", func(t *testing.T) {
+		got := Greet("", "")
 		want := "Hello, World"
+		assertion(t, got, want)
+	})
+
+	t.Run("should greet someone in spanish", func(t *testing.T) {
+		got := Greet("Joana", "Spanish")
+		want := "Hola, Joana"
+		assertion(t, got, want)
+	})
+
+	t.Run("Should greet someone in French", func(t *testing.T) {
+		got := Greet("Marcel", "French")
+		want := "Bonjour, Marcel"
 		assertion(t, got, want)
 	})
 }
