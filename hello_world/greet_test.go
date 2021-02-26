93b790b2064f6aa454ab93028b086c8aa8c1a93f
Ojini Chizoba Jude <judoc96@gmail.com>
2021-02-26 16:18:50 +0100

    refactor: group test and use helper function

--- hello_world/greet_test.go
+++ hello_world/greet_test.go
@@ -3,10 +3,23 @@ package main
 import "testing"
 
 func TestGreeting(t *testing.T) {
-	got := Greet("Jude")
-	want := "Hello, Jude"
+	assertion := func(t testing.TB, got, want string) {
+		t.Helper()
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("Greet someone (Jude)", func(t *testing.T) {
+		got := Greet("Jude")
+		want := "Hello, Jude"
+		assertion(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when empty string is supplied", func(t *testing.T) {
+		got := Greet("")
+		want := "Hello, World"
+		assertion(t, got, want)
+	})
 }
