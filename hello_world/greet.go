410099cb3af86f5a8529a00cf8bbd1024a655da0
Ojini Chizoba Jude <judoc96@gmail.com>
2021-02-26 17:05:56 +0100

    extract out greeting and generatePrefix functionality, and use switch statement

--- hello_world/greet.go
+++ hello_world/greet.go
@@ -10,18 +10,34 @@ const (
 	french             = "French"
 )
 
+func greeting(prefix, name string) string {
+	return prefix + name
+}
+
+func generatePrefix(lang string) (prefix string) { //default value for prefix is empty string ""
+	switch lang {
+	case spanish:
+		prefix = spanishHelloPrefix
+		break
+	case french:
+		prefix = frenchHelloPrefix
+		break
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	// another way of returning the prefix is by
+	// just specifying return, prefix will be returned automatically
+	return prefix
+}
+
 // Greet the user
 func Greet(name string, lang string) string {
 	if name == "" {
 		name = "World"
 	}
-	if lang == spanish {
-		return spanishHelloPrefix + name
-	}
-	if lang == french {
-		return frenchHelloPrefix + name
-	}
-	return englishHelloPrefix + name
+	prefix := generatePrefix(lang)
+	return greeting(prefix, name)
 }
 
 func main() {
